f470f445dc21b04cbcd2e94b48e16380cefb6f28
Jordan Zdimirovic <jordan@Jordans-MacBook-Pro.local>
2025-08-17 13:09:22 +1000

    Next will rewrite api calls to port 3000

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return process.env.NODE_ENV === "production" ? [] : [
+      { source: '/api/:path*', destination: 'http://localhost:3000/:path*' },
+    ];
+  },
+};
 
 module.exports = nextConfig;
+
